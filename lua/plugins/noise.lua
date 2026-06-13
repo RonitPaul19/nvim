f6bcdc803a962af76563045129b04f1034212a20
RonitPaul19 <ronitpaulindia@gmail.com>
2026-06-13 12:22:03 +0530

    added cmd line feature to appear in the center

--- lua/plugins/noise.lua
+++ lua/plugins/noise.lua
@@ -1,14 +1,19 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  opts = {},
   dependencies = {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
   },
-  config = function(_, opts)
+  config = function()
     require("notify").setup({
-      background_colour = "NONE"
+      background_colour = "NONE",
+    })
+
+    require("noice").setup({
+      cmdline = {
+        view = "cmdline_popup",
+      },
     })
   end,
 }
