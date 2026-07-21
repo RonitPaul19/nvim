f6ee7b22e5b6b146e7f6f822d4697a2004679450
RonitPaul19 <ronitpaulindia@gmail.com>
2026-07-22 00:45:06 +0530

    added better harpoon integration code with lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,49 +4,59 @@ return {
     "nvim-tree/nvim-web-devicons",
     "ThePrimeagen/harpoon",
   },
-
   config = function()
-    local harpoon = require("harpoon")
+    local colors = {
+      foam = "#9ccfd8",
+      base = "#191724",
+      white = "#ffffff",
+    }
 
-    -- Active Harpoon item (Rose Pine Foam)
-    vim.api.nvim_set_hl(0, "HarpoonActive", {
-      fg = "#191724", -- Base
-      bg = "#9ccfd8", -- Foam
-      bold = true,
-    })
-
-    -- Inactive Harpoon items (transparent)
-    vim.api.nvim_set_hl(0, "HarpoonInactive", {
-      fg = "#e0def4", -- Text
-      bg = "NONE",
-    })
-
-    local function harpoon_files()
-      local list = harpoon:list()
-      local current = vim.api.nvim_buf_get_name(0)
-      local result = {}
-
-      for i = 1, list:length() do
-        local item = list:get(i)
-
-        if item and item.value then
-          local name = vim.fn.fnamemodify(item.value, ":t")
-
-          if item.value == current then
-            table.insert(
-              result,
-              "%#HarpoonActive# " .. i .. ":" .. name .. " %#Normal#"
-            )
-          else
-            table.insert(
-              result,
-              "%#HarpoonInactive# " .. i .. ":" .. name .. " %#Normal#"
-            )
+    -- Build one lualine component per harpoon slot
+    local function harpoon_component(index)
+      return {
+        function()
+          local ok, harpoon = pcall(require, "harpoon")
+          if not ok then
+            return ""
           end
-        end
-      end
+          local item = harpoon:list():get(index)
+          if not item then
+            return ""
+          end
+          return " " .. vim.fn.fnamemodify(item.value, ":t") .. " "
+        end,
+        color = function()
+          local ok, harpoon = pcall(require, "harpoon")
+          if not ok then
+            return { bg = "NONE", fg = colors.white }
+          end
+          local item = harpoon:list():get(index)
+          if not item then
+            return { bg = "NONE", fg = colors.white }
+          end
 
-      return table.concat(result, " ")
+          local full_path = vim.fn.fnamemodify(item.value, ":p")
+          local current_file = vim.fn.expand("%:p")
+
+          if full_path == current_file then
+            return { bg = colors.foam, fg = colors.base, gui = "bold" }
+          else
+            return { bg = "NONE", fg = colors.white }
+          end
+        end,
+        cond = function()
+          local ok, harpoon = pcall(require, "harpoon")
+          if not ok then
+            return false
+          end
+          return harpoon:list():get(index) ~= nil
+        end,
+      }
+    end
+
+    local harpoon_slots = {}
+    for i = 1, 4 do -- adjust if you keep more than 4 harpoon marks
+      table.insert(harpoon_slots, harpoon_component(i))
     end
 
     require("lualine").setup({
@@ -56,9 +66,8 @@ return {
         section_separators = "",
         globalstatus = true,
       },
-
       sections = {
-        lualine_a = { harpoon_files },
+        lualine_a = harpoon_slots,
         lualine_b = {},
         lualine_c = {},
         lualine_x = { "filetype" },
