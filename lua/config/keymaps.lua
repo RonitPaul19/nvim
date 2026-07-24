9a089d65d3942bbfa01ef8ab65d2a9099e6e2910
RonitPaul19 <ronitpaulindia@gmail.com>
2026-07-24 20:34:25 +0530

    added comments for better navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,23 +3,35 @@ vim.g.maplocalleader = " "
 
 local set = vim.keymap.set
 
+-- Save
 set('n', '<leader>s', '<cmd>w<CR>')
 
+-- Delete and paste without yanking
 set('x', 'p', [["_dP]], { desc = "Paste without yanking" })
 set({ 'n', 'v' }, 'd', [["_d]], { desc = "Delete without yanking" })
 
+-- Redo
 set('n', 'U', '<C-r>')
+
+-- nohl
 set('n', '<Esc>', '<cmd>nohlsearch<CR>', { silent = true })
+
+-- skip folds
 set('n', 'j', 'gj', { noremap = true, silent = true })
 set('n', 'k', 'gk', { noremap = true, silent = true })
+
+-- quit
 set('n', '<leader>q', '<cmd>q<CR>')
 
+-- centered page up-down
 set('n', '<C-d>', '<C-d>zz', { noremap = true })
 set('n', '<C-u>', '<C-u>zz', { noremap = true })
 
+-- centered next and Previous search
 set("n", "n", "nzzzv", { desc = "Next search result cursor centered" })
 set("n", "N", "Nzzzv", { desc = "Previous search result cursor centered" })
 
+-- restart neovim
 set('n', '<leader>rw', '<cmd>restart<CR>', { desc = "restart neovim" })
 
 -- visual block mode
@@ -42,5 +54,6 @@ set("n", "<C-j>", "<C-w>j")
 set("n", "<C-k>", "<C-w>k")
 set("n", "<C-l>", "<C-w>l")
 
+-- Creating indents
 set("v", "<", "<gv")
 set("v", ">", ">gv")
