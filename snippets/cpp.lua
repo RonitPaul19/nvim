f5833500f1be21def2a05a9d91e628b50d06afac
RonitPaul19 <ronitpaulindia@gmail.com>
2026-08-03 02:02:32 +0530

    made changes in the c++ boilerplate code

--- snippets/cpp.lua
+++ snippets/cpp.lua
@@ -6,19 +6,18 @@ local i = ls.insert_node
 
 return {
 
-  s("cppmain", {
-    t({
-      "#include <bits/stdc++.h>",
-      "using namespace std;",
-      "",
-      "int main() {",
-      "  ",
-    }),
-    i(1),
-    t({
-      "",
-      "  return 0;",
-      "}",
-    }),
-  }),
+	s("cppmain", {
+		t({
+			"#include <iostream>",
+			"",
+			"int main() {",
+			"  ",
+		}),
+		i(1),
+		t({
+			"",
+			"  return 0;",
+			"}",
+		}),
+	}),
 }
