1469c04bf57316c8d39d0e5fb74d6e076327bf8e
RonitPaul19 <ronitpaulindia@gmail.com>
2026-08-03 20:17:07 +0530

    added different languages in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,39 +1,46 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    ensure_installed = {
-      "c",
-      "cpp",
-      "css",
-      "go",
-      "java",
-      "javascript",
-      "php",
-      "python",
-      "rust",
-    },
-    highlight = {
-      enable = true,
-    },
-    indent = {
-      enable = true,
-    },
-  },
-  config = function(_, opts)
-    -- Force treesitter to use gcc instead of cl.exe (Windows MSVC workaround)
-    -- Also set CC env var so tree-sitter CLI uses gcc (not cl.exe)
-    vim.env.CC = "gcc"
-    require("nvim-treesitter.install").compilers = { "gcc" }
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		ensure_installed = {
+			"lua",
+			"vim",
+			"vimdoc",
 
-    require("nvim-treesitter").setup(opts)
+			"typescript",
+			"tsx",
 
-    vim.filetype.add({
-      extension = {
-        mdx = "mdx",
-      },
-    })
-    vim.treesitter.language.register("markdown", "mdx")
-  end,
+			"c",
+			"cpp",
+			"css",
+			"go",
+			"java",
+			"javascript",
+			"php",
+			"python",
+			"rust",
+		},
+		highlight = {
+			enable = true,
+		},
+		indent = {
+			enable = true,
+		},
+	},
+	config = function(_, opts)
+		-- Force treesitter to use gcc instead of cl.exe (Windows MSVC workaround)
+		-- Also set CC env var so tree-sitter CLI uses gcc (not cl.exe)
+		vim.env.CC = "gcc"
+		require("nvim-treesitter.install").compilers = { "gcc" }
+
+		require("nvim-treesitter").setup(opts)
+
+		vim.filetype.add({
+			extension = {
+				mdx = "mdx",
+			},
+		})
+		vim.treesitter.language.register("markdown", "mdx")
+	end,
 }
