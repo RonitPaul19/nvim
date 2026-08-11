19249cc93af90a7bfd94a21b84e73d34795752be
RonitPaul19 <ronitpaulindia@gmail.com>
2026-08-11 23:26:23 +0530

    fixed duplicate mappings

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -17,7 +17,7 @@ return {
 		config = function()
 			require("gitsigns").setup({})
 
-			vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>")
+			vim.keymap.set("n", "<leader>gh", "<cmd>Gitsigns preview_hunk<cr>")
 		end,
 	},
 }
