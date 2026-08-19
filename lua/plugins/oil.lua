471985593c3ba79e356149e8b8bcd91057bd1d14
RonitPaul19 <ronitpaulindia@gmail.com>
2026-08-19 22:58:27 +0530

    starting oil on neovim startup

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,11 +1,8 @@
 return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	cmd = { "Oil" },
-	keys = {
-		{ "<leader>pv", "<cmd>Oil<CR>", desc = "Open parent directory" },
-	},
 	config = function()
+    vim.keymap.set("n", "<leader>pv", "<cmd>Oil<CR>")
 		require("oil").setup({
 			default_file_explorer = true,
 			show_hidden = true,
