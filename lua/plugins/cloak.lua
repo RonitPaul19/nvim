6a861343a5e4aecd8ee5fc832ed8115bcd4374aa
RonitPaul19 <ronitpaulindia@gmail.com>
2026-08-29 22:14:31 +0530

    added cloak for better privacy

---
+++ lua/plugins/cloak.lua
@@ -0,0 +1,22 @@
+return {
+	"laytan/cloak.nvim",
+	event = { "BufReadPost", "BufNewFile" },
+	config = function()
+		require("cloak").setup({
+			enabled = true,
+			cloak_character = "*",
+			highlight_group = "Comment",
+
+			patterns = {
+				{
+					file_pattern = {
+						".env*",
+						"wrangler.toml",
+						".dev.vars",
+					},
+					cloak_pattern = "=.+",
+				},
+			},
+		})
+	end,
+}
